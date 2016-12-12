d48e1cb788a5323fa61333fbe8ad8c36117f7b5d
Subramanian Alagappan <subbalam123@yahoo.co.in>
2016-12-12 19:09:01 +0100

    phone test 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-//google.maps.event.addDomListeNWr(window, 'load', init);
+//google.maps.event.addDomListener(window, 'load', init);
 // function mapInit() {
 //                // Basic options for a simple Google Map
 //                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 //                   if(navigator.geolocation) {
 //                     navigator.geolocation.getCurrentPosition(function (position) {
-//                       initialLocation = NWw google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//                       initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 //                       map.setCenter(initialLocation);
 //                     });
 //                   }
@@ -16,7 +16,7 @@
 //                    zoom: 14,
 //
 //                    // The latitude and longitude to center the map (always required)
-//                    center: NWw google.maps.LatLng(59.3269414, 18.0681448), // Stockholm
+//                    center: new google.maps.LatLng(59.3269414, 18.0681448), // Stockholm
 //
 //
 //
@@ -24,29 +24,29 @@
 //
 //                    // How you would like to style the map.
 //                    // This is where you would paste any style found on Snazzy Maps.
-//                    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightNWss":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.liNW","elementType":"geometry","stylers":[{"visibility":"on"},{"lightNWss":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]               };
+//                    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]               };
 //
 //                // Get the HTML DOM element that will contain your map
 //                // We are using a div with id="map" seen below in the <body>
 //                var mapElement = document.getElementById('map');
 //
-//                // Create the Google Map using our element and options defiNWd above
-//                var map = NWw google.maps.Map(mapElement, mapOptions);
+//                // Create the Google Map using our element and options defined above
+//                var map = new google.maps.Map(mapElement, mapOptions);
 //
 //                /* Let's also add a marker while we're at it
-//                var marker = NWw google.maps.Marker({
-//                    position: NWw google.maps.LatLng(59.3269414, 18.0681448),
+//                var marker = new google.maps.Marker({
+//                    position: new google.maps.LatLng(59.3269414, 18.0681448),
 //                    map: map,
 //                    title: 'You are here!'
 //                });
 //                */
 //            }
 
-document.addEventListeNWr("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-	if (window.DeviceOrientatioNWvent) {
+	if (window.DeviceOrientationEvent) {
 
-    window.addEventListeNWr('deviceorientation', function(eventData) {
+    window.addEventListener('deviceorientation', function(eventData) {
     	// gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
       // gamma: Het kantelen van links naar rechts in graden. Naar rechts kantelen zal een positieve waarde geven.
       var tiltLR = eventData.gamma;
@@ -66,13 +66,13 @@ document.addEventListeNWr("DOMContentLoaded", function(event) {
     }, false);
   }
   else {
-    document.getElementById("heading").inNWrHTML = "Helaas. De DeviceOrientatioNWvent API word niet door dit toestel ondersteund."
+    document.getElementById("heading").innerHTML = "Helaas. De DeviceOrientationEvent API word niet door dit toestel ondersteund."
   };
 
     function deviceOrientationHandler(tiltLR, tiltFB, dir) {
-      document.getElementById("tiltLR").inNWrHTML = Math.ceil(tiltLR);
-      document.getElementById("tiltFB").inNWrHTML = Math.ceil(tiltFB);
-      document.getElementById("direction").inNWrHTML = Math.ceil(dir);
+      document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
+      document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
+      document.getElementById("direction").innerHTML = Math.ceil(dir);
 
       // Rotate the disc of the compass.
       // Laat de kompas schijf draaien.
@@ -85,7 +85,7 @@ document.addEventListeNWr("DOMContentLoaded", function(event) {
 });
 function calcDirection(heading){
   if(heading >= 0 && heading < 90){
-    return 'NW';
+    return 'NE';
   }
   else if(heading >= 90 && heading < 180){
     return 'SE';
@@ -99,13 +99,13 @@ function calcDirection(heading){
 }
 
 function updateCircle(direction){
-  if(direction == 'NW'){
-    var largest = Math.max.apply(Math, [Data.NW.history, Data.NW.art, Data.NW.cafe, Data.NW.restaurant, Data.NW.stores]);
-    $('.category.cat1').css("border-top-color","rgba(231, 76, 60,"+Data.NW.history/largest+")");
-    $('.category.cat2').css("border-top-color","rgba(155, 89, 182,"+Data.NW.art/largest+")");
-    $('.category.cat3').css("border-top-color","rgba(241, 196, 15,"+Data.NW.cafe/largest+")");
-    $('.category.cat4').css("border-top-color","rgba(41, 128, 185,"+Data.NW.restaurant/largest+")");
-    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.NW.stores/largest+")");
+  if(direction == 'NE'){
+    var largest = Math.max.apply(Math, [Data.NE.history, Data.NE.art, Data.NE.cafe, Data.NE.restaurant, Data.NE.stores]);
+    $('.category.cat1').css("border-top-color","rgba(231, 76, 60,"+Data.NE.history/largest+")");
+    $('.category.cat2').css("border-top-color","rgba(155, 89, 182,"+Data.NE.art/largest+")");
+    $('.category.cat3').css("border-top-color","rgba(241, 196, 15,"+Data.NE.cafe/largest+")");
+    $('.category.cat4').css("border-top-color","rgba(41, 128, 185,"+Data.NE.restaurant/largest+")");
+    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.NE.store/largest+")");
   }
   else if(direction == 'SE'){
     var largest = Math.max.apply(Math, [Data.SE.history, Data.SE.art, Data.SE.cafe, Data.SE.restaurant, Data.SE.stores]);
@@ -113,7 +113,7 @@ function updateCircle(direction){
     $('.category.cat2').css("border-top-color","rgba(155, 89, 182,"+Data.SE.art/largest+")");
     $('.category.cat3').css("border-top-color","rgba(241, 196, 15,"+Data.SE.cafe/largest+")");
     $('.category.cat4').css("border-top-color","rgba(41, 128, 185,"+Data.SE.restaurant/largest+")");
-    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.SE.stores/largest+")");
+    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.SE.store/largest+")");
   }
   else if(direction == 'NW'){
     var largest = Math.max.apply(Math, [Data.NW.history, Data.NW.art, Data.NW.cafe, Data.NW.restaurant, Data.NW.stores]);
@@ -121,7 +121,7 @@ function updateCircle(direction){
     $('.category.cat2').css("border-top-color","rgba(155, 89, 182,"+Data.NW.art/largest+")");
     $('.category.cat3').css("border-top-color","rgba(241, 196, 15,"+Data.NW.cafe/largest+")");
     $('.category.cat4').css("border-top-color","rgba(41, 128, 185,"+Data.NW.restaurant/largest+")");
-    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.NW.stores/largest+")");
+    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.NW.store/largest+")");
   }
   else if(direction == 'SW'){
     var largest = Math.max.apply(Math, [Data.SW.history, Data.SW.art, Data.SW.cafe, Data.SW.restaurant, Data.SW.stores]);
@@ -129,7 +129,7 @@ function updateCircle(direction){
     $('.category.cat2').css("border-top-color","rgba(155, 89, 182,"+Data.SW.art/largest+")");
     $('.category.cat3').css("border-top-color","rgba(241, 196, 15,"+Data.SW.cafe/largest+")");
     $('.category.cat4').css("border-top-color","rgba(41, 128, 185,"+Data.SW.restaurant/largest+")");
-    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.SW.stores/largest+")");
+    $('.category.cat5').css("border-top-color","rgba(230, 126, 34,"+Data.SW.store/largest+")");
   }
 }
 $(document).ready(function(){
@@ -143,22 +143,22 @@ $(document).ready(function(){
 	 });
 
 	 $("#backBtn").click(function(){
-		 $("#reviewScreen").css("display", "noSE");
+		 $("#reviewScreen").css("display", "none");
 		 $("#compassScreen").css("display", "block");
 	 });
 
 	 $("#speakerBtn1").click(function(){
-		 $("#speakerBtn1").css("display", "noNW");
+		 $("#speakerBtn1").css("display", "none");
 		 $("#speakerBtn2").css("display", "block");
 	 });
 	 $("#speakerBtn2").click(function(){
-		 $("#speakerBtn2").css("display", "noNW");
+		 $("#speakerBtn2").css("display", "none");
 		 $("#speakerBtn1").css("display", "block");
 	 });
 });
 
 var Data = {
-  'NW': {
+  'NE': {
     'history': 20,
     'art': 10,
     'cafe': 5,
